2bb511391908b6d49796c1f0b5e86b6a4577c025
Juan <78928643+jNNx@users.noreply.github.com>
2022-11-25 11:23:27 -0300

    Update ejercicio1.js
    Finalizados los primeros 40 ejercicios de algoritmos

--- AyED/Ejercicio1.js
+++ AyED/Ejercicio1.js
@@ -165,4 +165,65 @@
     let myStr7 = "Learning to code is ";
     myStr7 += someAdjective;
 
-//Ej32: 
+//Ej32: Use the .length property to set lastNameLength to the number of characters in lastName.
+    // Setup
+    let lastNameLength = 0;
+    const lastName = "Lovelace";
+    // Only change code below this line
+    lastNameLength = lastName.length;
+    console.log(lastNameLength);
+
+//Ej33: Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+    // Setup
+    let firstLetterOfLastName = "";
+    const lastName2 = "Lovelace";
+
+    // Only change code below this line
+    firstLetterOfLastName = lastName2[0]; // Change this line
+
+//Ej34: Correct the assignment to myStr so it contains the string value of Hello World using the approach shown in the example above.
+    // Setup
+    let myStr8 = "Jello World";
+    // Only change code below this line
+    myStr8 = "Hello World"; // Change this line
+    // Only change code above this line
+
+//Ej35: Let's try to set thirdLetterOfLastName to equal the third letter of the lastName variable using bracket notation.
+    // Setup
+    const lastName3 = "Lovelace";
+    // Only change code below this line
+    const thirdLetterOfLastName = lastName3[2]; // Change this line
+
+//Ej36: Use bracket notation to find the last character in the lastName variable.
+    // Setup
+    const lastName4 = "Lovelace";
+    // Only change code below this line
+    const lastLetterOfLastName = lastName4[lastName4.length-1]; // Change this line
+
+//Ej37: Use bracket notation to find the second-to-last character in the lastName string.
+    // Setup
+    const lastName5 = "Lovelace";
+    // Only change code below this line
+    const secondToLastLetterOfLastName = lastName5[lastName5.length - 2]; // Change this line
+
+/*Ej38: In this challenge, we provide you with a noun, a verb, an adjective and an adverb. You need to form a complete sentence using words of your choice, along with the words we provide.
+        You will need to use the string concatenation operator + to build a new string, using the provided variables: myNoun, myAdjective, myVerb, and myAdverb. 
+        You will then assign the formed string to the wordBlanks variable. You should not change the words assigned to the variables.
+        You will also need to account for spaces in your string, so that the final sentence has spaces between all the words. The result should be a complete sentence.*/
+    const myNoun = "dog";
+    const myAdjective = "big";
+    const myVerb = "ran";
+    const myAdverb = "quickly";
+    // Only change code below this line
+    const wordBlanks = "I saw a " + myNoun + " and it was so " + myAdjective + " that scared me and I start to " + myVerb + " very " +myAdverb; // Change this line
+    console.log(wordBlanks)
+    // Only change code above this line
+
+//Ej39: Modify the new array myArray so that it contains both a string and a number (in that order).
+    // Only change code below this line
+    const myArray = ["hello", 7];
+
+//Ej40: Create a nested array called myArray.
+    // Only change code below this line
+    const myArray2 = [["Messi", 10], ["Cristiano", 7]];
+
